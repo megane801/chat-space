784e06d95d6f1e4bb0775391f82f798babf7cc8f
megane801 <62549481+megane801@users.noreply.github.com>
2020-04-04 20:42:17 +0900

    自動更新

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,7 +2,7 @@ $(function(){
   function buildHTML(message){
     if ( message.image ) {
       var html =
-       `<div class="message">
+       `<div class="message" data-message-id=${message.id}>
           <div class="messages-info">
             <div class="messages-user">
               ${message.user_name}
@@ -21,7 +21,7 @@ $(function(){
       return html;
     } else {
       var html =
-       `<div class="message">
+       `<div class="message" data-message-id=${message.id}>
           <div class="messages-info">
             <div class="messages-user">
               ${message.user_name}
@@ -44,6 +44,7 @@ $(function(){
     e.preventDefault();
     var formData = new FormData(this);
     var url = $(this).attr('action')
+
     $.ajax({
       url: url,
       type: 'POST',  
@@ -54,6 +55,7 @@ $(function(){
     })
     .done(function(data){
       var html = buildHTML(data);
+      $('.submit-btn').attr('disabled', false);
       $('.chat-main__messagelist').append(html);
       $('.chat-main__messagelist').animate({ scrollTop: $('.chat-main__messagelist')[0].scrollHeight});
       $('form')[0].reset();
@@ -61,6 +63,34 @@ $(function(){
     .fail(function() {
       alert("メッセージ送信に失敗しました");
     });
-    return false;
   });
+
+  var reloadMessages = function() {
+    var last_message_id = $('.message:last').data("message-id");
+    
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      if (messages.length !== 0) {
+        var insertHTML = '';
+        $.each(messages, function(i, message){
+          insertHTML += buildHTML(message)
+        });
+        $('.chat-main__messagelist').append(insertHTML);
+        $('.chat-main__messagelist').animate({ scrollTop: $('.chat-main__messagelist')[0].scrollHeight});
+        $('.submit-btn').attr('disabled', false);
+      }
+    })
+    .fail(function() {
+      alert('error');
+    });
+  }
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
+  return false;
 });
